93a050a83fbeb71dda103ef495b6ab5c686afdcd
badrmohamed <badr.mohammed@buseet.com>
2022-05-31 21:34:43 +0200

    finished weather app

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,26 @@ import 'react-loading-skeleton/dist/skeleton.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import LayoutController from './routing/Layout/LayoutController';
-import ErrorBoundary from './modules/ErrorBoundary';
 import { store } from './redux/store';
 import { changeLanguage, getLanguage } from './lang/local';
 import { SkeletonTheme } from 'react-loading-skeleton';
 
 function App() {
   return (
-    <ErrorBoundary>
-      <SkeletonTheme width='100%' height='100%' borderRadius='inherit' baseColor='#fff'>
-        <Provider store={store}>
-          <BrowserRouter>
-            <Switch>
-              <Route render={(rest) => <LayoutController {...rest} />} />
-            </Switch>
-          </BrowserRouter>
-        </Provider>
-      </SkeletonTheme>
-    </ErrorBoundary>
+    <SkeletonTheme
+      width='100%'
+      height='100%'
+      borderRadius='inherit'
+      baseColor='#fff'
+    >
+      <Provider store={store}>
+        <BrowserRouter>
+          <Switch>
+            <Route render={(rest) => <LayoutController {...rest} />} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
+    </SkeletonTheme>
   );
 }
 changeLanguage();
